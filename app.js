8634d511ecdca1fdf502e2378d78a2e977dcc99f
Mark-hernandez <Mark-hernandez@outlook.com>
2016-12-08 16:26:48 -0800

    inital upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var quiz = {
+	question: "What color are coffee beans before they are roasted?"
+	options: ['White', 'Brown', 'Green', 'Orange',],
+	answer: "Green"
+},
+/*
+	question: "On average, How many cups are consumed by Americans each day?"
+	options: ['100 million'. '235 Million', '400 Million', '587 Million']
+	answer: "400 Million"
+},
+{
+	question: "What are the two main types of coffee beans?",
+	options: ['Robusta and Augusta', 'Arabica and Aromatic', 'Caffeine and Green', 'Arabica and Robusta'],
+	answer: "Arabica and Robusta"
+},
+{
+	question: "Where does coffee originate from?"
+	options: ['Columbia', 'Argentina', 'Hawaii', 'Ethiopia'],
+	answer: 'Ethiopia'
+},
+{
+	question: "What country is the leading coffee consumer?"
+	options: ['Italy', 'United States', 'Italy', 'Columbia'],
+	answer: "United States"
+},
+{
+	question: "What country is most produced in?"
+	options: ['Brazil', 'Nicaragua', 'El Salvador', 'Columbia']
+	answer: "Columbia"
+},
+{
+	question: "The worlds most expensive coffee is..."
+	options: ['$60/lbs', '$120/lbs', '$270/lbs', '$600/lbs'],
+	answer: "$600/lbs"
+},
+{
+	question: "You would need to drink __ cups of coffee to overdose on Caffeine and die."
+	options:[ 30, 57, 594, 100],
+	answer:"100 cups"
+},
+{
+	question: "Coffee stays warmer longer when you add?"
+	options:['Cream', 'Sugar', 'Honey', 'Tea'],
+	answer:"Cream"
+},
+{
+	question:"The biggest cup of coffee ever brewed in 2014 in South Korea measured"
+	options: ['300 Gallons', '500 Gallons', '80,000 Gallons', '3,700 Gallons']
+	answer: "3,700 Gallons"
+},
+}
+*/
+var questionNumber = [];
+var questionOptions = [];
+
